f0462da2a4ddb146cd9fa94874ab2dd0099dd8eb
Tom Burdick <tburdick@wrightwoodtech.com>
2011-04-26 15:42:37 -0500

    pool api mostly done, lots of stuff to flesh out still though

--- src/ht_pool.erl
+++ src/ht_pool.erl
@@ -1,16 +1,81 @@
 %% @author Tom Burdick <thomas.burdick@gmail.com>
 %% @copyright 2011 Tom Burdick
-%% @doc Hot Tub Pool State Machine.
+%% @doc Hot Tub Pool Balancer.
 
 -module(ht_pool).
 
--behaviour(gen_fsm).
+-behaviour(gen_server).
 
 %% api
--export([start_link/3, with_worker/2]).
+-export([start_link/2, with_worker/2]).
 
-%% gen_fsm callbacks
--export([init/1, initialized/2, started/2, handle_event/3,
-    handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {sup=undefined, pool_procs=queue:new()}).
+-record(state, {workers=dict:new()}).
+
+
+%% ----------------------------------------------------------------------------
+%% api
+%% ----------------------------------------------------------------------------
+
+%% @doc Start a linked pool manager.
+-spec start_link(Name::atom(), Limit::pos_integer()) -> {ok, pid()}.
+start_link(Name, Limit) ->
+    gen_server:start_link({local, Name}, [Limit], []).
+
+%% @doc Perform a function with a worker process.
+-spec with_worker(Name::atom(), Fun::fun()) -> any().
+with_worker(Name, Fun) ->
+    {ok, Worker} = checkout_worker(Name),
+    try
+        Fun(Worker)
+    after
+        checkin_worker(Name, Worker)
+    end.
+
+%% ------------------------------------------------------------------
+%% private api 
+%% ------------------------------------------------------------------
+checkout_worker(Name) ->
+    gen_server:call(Name, {checkout}).
+
+checkin_worker(Name, Worker) ->
+    gen_server:cast(Name, {checkin, Worker}).
+
+%% @doc Find the least used worker in the pool.
+unused_worker(Key, Value, undefined) ->
+    {Key, Value};
+unused_worker(_Key, Value, {OKey, OValue}) when OValue < Value ->
+    {OKey, OValue}.
+
+%% ------------------------------------------------------------------
+%% gen_server callbacks
+%% ------------------------------------------------------------------
+
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call({checkout}, _From, State) ->
+    {Worker, _Checkouts} = dict:fold(fun unused_worker/3, State#state.workers),
+    Workers = dict:update_counter(Worker, 1, State#state.workers),
+    {reply, Worker, State#state{workers=Workers}}.
+
+handle_cast({checkin, Worker}, State) ->
+    case dict:is_key(Worker) of
+        true ->
+            Workers = dict:update_counter(Worker, -1, State#state.workers),
+            {noreply, State#state{workers=Workers}};
+        false ->
+            {noreply, State}
+    end.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
--- src/ht_sup.erl
+++ src/ht_sup.erl
@@ -21,10 +21,28 @@
 -spec start_link(Name::atom(), Limit::pos_integer(), Module::module(),
     Function::function(), Arguments::list()) -> {ok, Sup::pid()}.
 start_link(Name, Limit, Module, Function, Arguments) ->
-    {ok, Sup} = supervisor:start_link({local, Name}, ?MODULE, []),
-    {ok, WorkSup} = start_worker_sup(Sup, Name, Module, Function, Arguments),
-    {ok, Pool} = start_pool(Sup, WorkSup, Name, Min, Max),
+    Name1 = list_to_atom(atom_to_list(Name) ++ "_sup"),
+    {ok, Sup} = supervisor:start_link({local, Name1}, ?MODULE, []),
+    {ok, _WorkSup} = start_worker_sup(Sup, Name, Module, Function, Arguments),
+    {ok, _Pool} = start_pool(Sup, Name, Limit),
     {ok, Sup}.
+
+
+%% ----------------------------------------------------------------------------
+%% private api
+%% ----------------------------------------------------------------------------
+
+start_worker_sup(Sup, Name, Module, Function, Arguments) ->
+    Name1 = list_to_atom(atom_to_list(Name) ++ "_worker_sup"),
+    Spec = {Name1, {ht_worker_sup, start_link, [Name, Module, Function, Arguments]},
+            permanent, 2000, supervisor, [ht_worker_sup]},
+    supervisor:start_child(Sup, Spec).
+
+start_pool(Sup, Name, Limit) ->
+    Name1 = list_to_atom(atom_to_list(Name) ++ "_pool"),
+    Spec = {Name1, {ht_pool, start_link, [Name, Limit]},
+            permanent, 2000, worker, [ht_pool]},
+    supervisor:start_child(Sup, Spec).
 
 
 %% ----------------------------------------------------------------------------
