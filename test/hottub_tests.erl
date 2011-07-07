2e96b650b994093322b671bb63e5cc47dc204956
Tom Burdick <tburdick@wrightwoodtech.com>
2011-07-07 07:35:27 -0500

    added benchmark test

--- test/hottub_tests.erl
+++ test/hottub_tests.erl
@@ -15,3 +15,34 @@ pool_crash_test() ->
         end),
     ?debugHere,
     ok.
+
+%% Benchmark Pool Checkout/Checkin Test.
+pool_benchmark_test() ->
+    hottub:start(bench_pool, 100, test_worker, start_link, []),
+    BenchFun = fun() ->
+        hottub:execute(bench_pool,
+            fun(Worker) ->
+                test_worker:nothing(Worker)
+            end)
+    end,
+    ets:new(ht_stats, [bag, public, named_table,
+            {read_concurrency, true}, {write_concurrency, true}]),
+    BenchWorkers = lists:map(
+        fun(Id) ->
+            {ok, Pid} = benchmark_worker:start_link(Id, ht_stats),
+            benchmark_worker:perform(Pid, BenchFun, 10000),
+            Pid
+    end, lists:seq(0, 1000)),
+    lists:foreach(
+        fun(Pid) ->
+            benchmark_worker:done(Pid),
+            benchmark_worker:stop(Pid),
+        end, BenchWorkers),
+    {MinTime, MeanTime, MaxTime} = ets:foldl(
+        fun({Begin, End}, {Min, Mean, Avg}) ->
+            {0.0, 0.0, 0.0}
+        end, ht_stats),
+    ?debugVal(MinTime),
+    ?debugVal(MeanTime),
+    ?debugVal(MaxTime),
+    ok.
